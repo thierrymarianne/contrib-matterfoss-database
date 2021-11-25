4bc982ae1df8b43ab85afcac7ab13aa5d667a41b
Thierry Marianne <thierry@marianne.io>
2021-11-25 13:12:45 +0100

    Allow use of arbitrary connection information

--- start.sh
+++ start.sh
@@ -4,8 +4,9 @@ function start_matterfoss_database() {
     docker ps -a | grep Exited | awk '{print $1}' | xargs -I{} docker rm -f {}
     docker run --name matterfoss-postgres --network host \
         -e POSTGRES_HOST_AUTH_METHOD='trust' \
-        -e POSTGRES_DB=mattermost_test \
-        -e POSTGRES_USER=mmuser -e POSTGRES_PASSWORD=mostest \
+        -e POSTGRES_DB="${MATTERFOSS_DB_NAME}" \
+        -e POSTGRES_USER="${MATTERFOSS_DB_USERNAME}" \
+        -e POSTGRES_PASSWORD="${MATTERFOSS_DB_PASSWORD}" \
         -v $(pwd)/data:/var/lib/postgresql/data postgres:13-bullseye
 }
 start_matterfoss_database
