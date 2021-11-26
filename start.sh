7b42cd9c7973b52d85f255625cebfb1d8f879956
Thierry Marianne <thierry@marianne.io>
2021-11-26 11:04:21 +0100

    Run database container in detached mode by default

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 function start_matterfoss_database() {
-    docker ps -a | grep Exited | awk '{print $1}' | xargs -I{} docker rm -f {}
+    docker ps -a | grep 'matterf[oss\-post]gres' | awk '{print $1}' | xargs -I{} docker rm -f {}
     docker run --name matterfoss-postgres --network host \
+        -d \
         -e POSTGRES_HOST_AUTH_METHOD='trust' \
         -e POSTGRES_DB="${MATTERFOSS_DB_NAME}" \
         -e POSTGRES_USER="${MATTERFOSS_DB_USERNAME}" \
